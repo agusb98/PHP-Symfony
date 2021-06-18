6fb4ed1806845296c85a9901977411aa2ca5fa2f
agusb98 <agusszurdob@gmail.com>
2021-06-17 23:04:29 -0300

    cya gitignore

---
+++ README.txt
@@ -0,0 +1,32 @@
+# TIPS AT THE MOMENT TO CREATE SYMFONY PROYECT
+
+    Install Symfony, link: https://symfony.com/download
+
+    terminal:
+        cd C:/xampp/htdocs
+        symfony new proyect_name
+        symfony serve:start
+
+# DATABASE
+
+    .env
+        Set DATABASE_URL="mysql://root:@127.0.0.1:3306/Ejemplo?serverVersion=5.7"
+
+    terminal:
+        Agregar
+            php bin/console doctrine:database:create
+
+        Actualizar
+            php bin/console doctrine:schema:update --force
+
+# ADD ENTITY
+    terminal:
+        php bin/console make:entity EntityName
+
+        Usuario(es complejo, utiliza otra cosa..):
+            php bin/console make:entity user
+
+
+# ADD CONTROLLER
+    terminal:
+        php bin/console make:controller
---
+++ src/Entity/Product.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/* 
+    los "comentarios" que se encuentran abajo, no son comentario. 
+    Especifican como será la propiedad
+    Fijate que utiliza **
+*/
+
+/**
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $photo;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $status;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="products")
+     */
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
